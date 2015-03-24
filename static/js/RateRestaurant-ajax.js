f7b60274d075096104d199381a521cf19ddf4d9d
moyi <moyi1990liu@gmail.com>
2015-03-24 15:03:43 +0000

    Restaurant

--- static/js/RateRestaurant-ajax.js
+++
@@ -1,9 +0,0 @@
-$(document).ready( function() {
-   $('.likes').click(function(){
-    var comid;
-    comid = $(this).attr("data-comid");
-    $.get('/RateRestaurant/like_comment/', {comment_id: comid}, function(data){
-               $('#like_count'+comid).html(data);
-    });
-});
-});
